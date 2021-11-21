f188f368600e94130263afc44de71b88797ff7ad
Johnatan Modesto <johnatan.modesto@gmail.com>
2021-11-20 22:09:01 -0300

    use lint staged config file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Main from 'components/Main'
 
 const Home: NextPage = () => {
-  return <Main title="Boilerplate setup" />
+  return <Main title="Boilerplate setup title" />
 }
 
 export default Home

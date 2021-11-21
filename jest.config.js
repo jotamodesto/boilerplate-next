9671d3e6bf7c1413f7d90d3dcbb683749dd7cf56
Johnatan Modesto <johnatan.modesto@gmail.com>
2021-11-21 16:20:14 -0300

    Removed stories files from test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+    '!<roorDir>/src/**/*.stories.tsx',
     '!**/*.d.ts',
     '!**/node_modules/**'
   ],

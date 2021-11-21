49085d39389db731786b5754b85fa99437a7da2b
Johnatan Modesto <johnatan.modesto@gmail.com>
2021-11-20 21:45:16 -0300

    Test lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Main from 'components/Main'
 
 const Home: NextPage = () => {
-  return <Main title="Boilerplate setup title" />
+  return <Main title="Boilerplate setup" />
 }
 
 export default Home
